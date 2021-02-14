9e7791b9c5b1170906a0b0ac14ebbaf600332235
Aleksander Grabowski <grabowski.a@wp.eu>
2021-02-14 22:26:19 +0100

    mapa3x3 button

--- 49DR_2/Aplikacja/Aplikacja/Form1.Designer.cs
+++ 49DR_2/Aplikacja/Aplikacja/Form1.Designer.cs
@@ -28,201 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.TempWew = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.TempZew = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.Wilg = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.Cisn = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // panel1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(570, 48);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(151, 28);
-            this.comboBox1.TabIndex = 1;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(570, 139);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(151, 28);
-            this.comboBox2.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(570, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Port COM";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(570, 116);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 20);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Szybkość Transmisji ";
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(270, 270);
+            this.panel1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(548, 385);
+            this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Połącz";
+            this.button1.Size = new System.Drawing.Size(90, 90);
+            this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(653, 385);
+            this.button2.Location = new System.Drawing.Point(90, 0);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Rozłącz";
+            this.button2.Size = new System.Drawing.Size(90, 90);
+            this.button2.TabIndex = 0;
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // button3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 13);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 20);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Stacja pogodowa";
+            this.button3.Location = new System.Drawing.Point(180, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 90);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // label5
+            // button4
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 51);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(178, 20);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Temperatura Wewnętrzna";
+            this.button4.Location = new System.Drawing.Point(0, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 90);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = true;
             // 
-            // TempWew
+            // button5
             // 
-            this.TempWew.AutoSize = true;
-            this.TempWew.Location = new System.Drawing.Point(197, 51);
-            this.TempWew.Name = "TempWew";
-            this.TempWew.Size = new System.Drawing.Size(50, 20);
-            this.TempWew.TabIndex = 10;
-            this.TempWew.Text = "label4";
+            this.button5.Location = new System.Drawing.Point(90, 90);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 90);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = true;
             // 
-            // label6
+            // button6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(13, 70);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(172, 20);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Temperatura zewnętrzna";
+            this.button6.Location = new System.Drawing.Point(180, 90);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 90);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = true;
             // 
-            // TempZew
+            // button7
             // 
-            this.TempZew.AutoSize = true;
-            this.TempZew.Location = new System.Drawing.Point(197, 70);
-            this.TempZew.Name = "TempZew";
-            this.TempZew.Size = new System.Drawing.Size(50, 20);
-            this.TempZew.TabIndex = 12;
-            this.TempZew.Text = "label7";
+            this.button7.Location = new System.Drawing.Point(0, 180);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(90, 90);
+            this.button7.TabIndex = 0;
+            this.button7.UseVisualStyleBackColor = true;
             // 
-            // label8
+            // button8
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(13, 90);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(88, 20);
-            this.label8.TabIndex = 13;
-            this.label8.Text = "Wilgotność ";
+            this.button8.Location = new System.Drawing.Point(180, 180);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(90, 90);
+            this.button8.TabIndex = 0;
+            this.button8.UseVisualStyleBackColor = true;
             // 
-            // Wilg
+            // button9
             // 
-            this.Wilg.AutoSize = true;
-            this.Wilg.Location = new System.Drawing.Point(197, 90);
-            this.Wilg.Name = "Wilg";
-            this.Wilg.Size = new System.Drawing.Size(50, 20);
-            this.Wilg.TabIndex = 14;
-            this.Wilg.Text = "label9";
+            this.button9.Location = new System.Drawing.Point(90, 180);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(90, 90);
+            this.button9.TabIndex = 0;
+            this.button9.UseVisualStyleBackColor = true;
             // 
-            // label10
+            // button10
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(13, 110);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(168, 20);
-            this.label10.TabIndex = 15;
-            this.label10.Text = "Ciśnienie atmosferyczne";
-            // 
-            // Cisn
-            // 
-            this.Cisn.AutoSize = true;
-            this.Cisn.Location = new System.Drawing.Point(197, 110);
-            this.Cisn.Name = "Cisn";
-            this.Cisn.Size = new System.Drawing.Size(58, 20);
-            this.Cisn.TabIndex = 16;
-            this.Cisn.Text = "label11";
+            this.button10.Location = new System.Drawing.Point(51, 318);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(194, 29);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "Start";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(759, 426);
-            this.Controls.Add(this.Cisn);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.Wilg);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.TempZew);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.TempWew);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(340, 483);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label TempWew;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label TempZew;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label Wilg;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label Cisn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- 49DR_2/Aplikacja/Aplikacja/Form1.cs
+++ 49DR_2/Aplikacja/Aplikacja/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
+
 
 namespace Aplikacja
 {
@@ -14,12 +16,95 @@ namespace Aplikacja
     {
         public Form1()
         {
-            InitializeComponent();
+            Point EmptyPoint;
+            ArrayList images = new ArrayList();
+            public Form1()
+            {
+                EmptyPoint.X = 180;
+                EmptyPoint.Y = 180;
+                InitializeComponent();
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            foreach (Button b in panel1.Controls)
+                b.Enabled = true;
+
+            Image original = Image.FromFile(@"C:\Users\Aleksander\source\repos\49DR_2\Aplikacja\Imagess");
+
+            cropImageTomages(original, 270, 270);
+
+            AddImagesToButtons(images);
+
+
+
+                
+        }
+
+        private void AddImagesToButtons(object images)
+        {
+            int i = 0;
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+            arr = suffle(arr);
+
+            foreach (Button b in panel1.Controls)
+            {
+                if (i < arr.Length)
+                {
+                    b.Image = (Image)images[arr[i]];
+                    i++;
+                }
+            }
+
+        }
+
+        private int[] suffle(int[] arr)
+        {
+            Random rand = new Random();
+            arr = arr.OrderBy(x => rand.Next()).ToArray();
+
+            return arr;
+        }
+
+        private void cropImageTomages(Image original, int v1, int v2)
+        {
+            Bitmap bmp = new Bitmap(w, h);
+
+            Graphics graphic = Graphics.FromImage(bmp);
+
+            graphic.DrawImage(orginal, 0, 0, w, h);
+
+            graphic.Dispose();
+
+            int movr = 0, movd = 0;
+
+            for (int x = 0; x < 8; x++)
+            {
+                Bitmap piece = new Bitmap(90, 90);
+
+                for (int i = 0; i < 90; i++)
+                    for (int j = 0; j < 90; j++)
+                        piece.SetPixel(i, j,
+                            bmp.GetPixel(i + movr, j + movd));
+
+                images.Add(piece);
+
+                movr += 90;
+
+                if (movr == 270)
+                {
+                    movr = 0;
+                    movd += 90;
+                }
+            }
         }
     }
 }
